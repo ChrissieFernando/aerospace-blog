210f48ebb6b0f22819b9794a0425161de8cfef38
Chrissie Fernando <chrissiefernando@icloud.com>
2016-07-15 21:44:16 +0530

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-  #get 'about' , to: 'pages#about'
-
+  
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  
   resources :articles
-
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :categories, except: [:destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
