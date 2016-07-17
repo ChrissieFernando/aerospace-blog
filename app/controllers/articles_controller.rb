2ac9c1518c36f9a28d4a6c50d8234d6cb51f6e02
Chrissie Fernando <chrissiefernando@icloud.com>
2016-07-17 16:11:25 +0530

    aa

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
 
   def index
 
-    @articles = Article.order('created_at DESC').paginate(page: params[:page], per_page: 5)
+    @articles = Article.order('created_at DESC').paginate(page: params[:page], per_page: 3)
   end
 
   def new
