caebaea0f5fef233d264a17e31b23234329713a1
Chrissie Fernando <chrissiefernando@icloud.com>
2016-07-17 16:09:06 +0530

    aa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :require_admin, only: [:destroy]
   def index
-    @users = User.paginate(page: params[:page], per_page: 4)
+    @users = User.order('username ASC').paginate(page: params[:page], per_page: 4)
   end
 
   def new
