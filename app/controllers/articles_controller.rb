9f344f4275f93b200ae13b1780964fbfb4ba6469
Chrissie Fernando <chrissiefernando@icloud.com>
2016-07-19 18:09:21 +0530

    updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,10 +37,6 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      if !params[:comment]
-        @article.comment = @article.comment + " " + "\n by " + current_user.username
-        @article.save
-      end
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
