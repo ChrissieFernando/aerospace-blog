fca9e2879ad16bfd128595470fe05834261c0b5a
Chrissie Fernando <chrissiefernando@icloud.com>
2016-07-21 18:25:23 +0530

    picture + signup added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # # session[:user_id] = @user.id
-      flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      # redirect_to user_path(@user)
-      render 'new'
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to the Aerospace blog #{@user.username}"
+      redirect_to user_path(@user)
+      # render 'new'
     else
       render 'new'
     end
