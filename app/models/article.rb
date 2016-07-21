fd34bdf7abb832cb78d2a70ad87fe9638a838522
Chrissie Fernando <chrissiefernando@icloud.com>
2016-07-22 00:41:29 +0530

    error cleared

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,7 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10 }
   validates :user_id, presence: true
+  validates :files,uniqueness: true
   mount_uploader :files, FilesUploader
   validate :files_size
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
   has_secure_password
+    validates :picture,uniqueness: true
   mount_uploader :picture, PictureUploader
   validate :picture_size
 
